e92b027ab299e681a0a71cb901f35f771ee7a3c7
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-06-15 11:57:32 +0300

    Add columns to the cuisines table

--- db/migrate/20180611150639_create_cuisines.rb
+++ db/migrate/20180611150639_create_cuisines.rb
@@ -1,8 +1,9 @@
 class CreateCuisines < ActiveRecord::Migration[5.1]
   def change
     create_table :cuisines do |t|
-
-      t.timestamps
+      t.string :region
+      t.string :image
+      t.timestamps null: false
     end
   end
 end
