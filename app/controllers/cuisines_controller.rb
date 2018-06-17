c9b4bd17af5fa3ee0541a41aef49ee0794972c02
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-06-17 11:51:33 +0300

    Add index and show actions to the cuisines contoller

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,2 +1,10 @@
 class CuisinesController < ApplicationController
+  def index
+    @cuisines = Cuisine.all
+  end
+
+  def show
+    @cuisine = Cuisine.find(params[:id])
+    @recipes = @cuisine.recipes
+  end
 end
