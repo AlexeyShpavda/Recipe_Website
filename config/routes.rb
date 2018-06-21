a5310b3bcb79195f088d4f8574d7035013ef4e01
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-06-21 11:57:59 +0300

    Add the routes to the config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'cuisines#index'
+  get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
+  resources :users
   resources :cuisines, only: [:index, :show]
   resources :recipes, only: [:show, :edit, :update, :destroy]
 end
